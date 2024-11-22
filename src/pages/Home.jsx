39ae72f1f7835e74de6ec58b984a64e5115360da
Sero <kmcaltiz@gmail.com>
2024-11-22 12:54:17 +0800

    add about component to Home Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import PageLayout from "../components/PageLayout";
 import Hero from "../components/Hero";
+import About from "../components/About";
 
 const Home = () => {
   return (
     <>
       <Hero />
-      <hr className="w-1/2 border-accent mx-auto"/>
+      <hr className="w-1/2 border-accent mx-auto" />
+      <About />
+      <hr className="w-1/2 border-accent mx-auto" />
     </>
   );
 };
