4dedaaecd4b8c914117a363507add711be2f84b8
Sero <kmcaltiz@gmail.com>
2024-11-23 06:41:22 +0800

    create register form component

---
+++ src/components/RegisterForm.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import Button from "./Button";
+
+const RegisterForm = () => {
+  return (
+    <form onSubmit={(e) => e.preventDefault()} className="space-y-5">
+      <div>
+        <label className="font-medium">Name</label>
+        <input
+          type="text"
+          required
+          className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-primary shadow-sm rounded-lg"
+        />
+      </div>
+      <div>
+        <label className="font-medium">Email</label>
+        <input
+          type="email"
+          required
+          className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-primaryshadow-sm rounded-lg"
+        />
+      </div>
+      <div>
+        <label className="font-medium">Password</label>
+        <input
+          type="password"
+          required
+          className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-primary shadow-sm rounded-lg"
+        />
+      </div>
+      <div>
+        <label className="font-medium">Confirm Password</label>
+        <input
+          type="password"
+          required
+          className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-primary shadow-sm rounded-lg"
+        />
+      </div>
+      <Button text={"Create account"} onClick={() => console.log("clicked")} />
+    </form>
+  );
+};
+
+export default RegisterForm;
