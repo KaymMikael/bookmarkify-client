dad44603a3aa9b5996c42e22188a34dd4b1b4e51
Sero <kmcaltiz@gmail.com>
2024-11-22 13:12:48 +0800

    Move header to Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import Header from "./components/Header";
 
 const App = () => {
   return (
     <>
-      <Header />
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-import PageLayout from "../components/PageLayout";
 import Hero from "../components/Hero";
 import About from "../components/About";
+import Header from "../components/Header";
+
+
 
 const Home = () => {
   return (
     <>
+      <Header />
       <Hero />
       <hr className="w-1/2 border-accent mx-auto" />
       <About />
