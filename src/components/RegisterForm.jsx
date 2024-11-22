99cc78bd6f99d8cf8c5a9da66f2d90af33c810ac
Sero <kmcaltiz@gmail.com>
2024-11-23 06:44:00 +0800

    add space to class name

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -17,7 +17,7 @@ const RegisterForm = () => {
         <input
           type="email"
           required
-          className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-primaryshadow-sm rounded-lg"
+          className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-primary shadow-sm rounded-lg"
         />
       </div>
       <div>
