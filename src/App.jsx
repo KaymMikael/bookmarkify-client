b829158b84a98456f234f89470344af9df805750
Sero <kmcaltiz@gmail.com>
2024-11-23 17:39:22 +0800

    rename dashboard to dashboardpage

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,35 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import LogInPage from "./pages/LogInPage";
 import RegisterPage from "./pages/RegisterPage";
+import DashboardPage from "./pages/DashboardPage";
 
 const App = () => {
+  const routes = [
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/login",
+      element: <LogInPage />,
+    },
+    {
+      path: "/register",
+      element: <RegisterPage />,
+    },
+    {
+      path: "/dashboard",
+      element: <DashboardPage />,
+    },
+  ];
+
   return (
     <>
       <main>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<LogInPage />} />
-          <Route path="/register" element={<RegisterPage />} />
+          {routes.map((route) => (
+            <Route path={route.path} element={route.element} key={route.path}/>
+          ))}
         </Routes>
       </main>
     </>
---
+++ src/pages/DashboardPage.jsx
@@ -0,0 +1,13 @@
+import SideBar from "../components/SideBar";
+import DashboardContent from "../components/DashboardContent";
+
+const DashboardPage = () => {
+  return (
+    <section>
+      <SideBar />
+      <DashboardContent />
+    </section>
+  );
+};
+
+export default DashboardPage;
