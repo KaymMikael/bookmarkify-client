133797376a35cec3a39e32f75a15e6fec05e0401
Sero <kmcaltiz@gmail.com>
2024-11-23 17:39:32 +0800

    create sidebar and add it to dashboard

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,81 @@
+import { useState, useEffect } from "react";
+
+const SideBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 768) {
+        setIsOpen(true);
+      } else {
+        setIsOpen(false);
+      }
+    };
+
+    handleResize(); // Set the initial state based on the current window size
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return (
+    <>
+      <span
+        className="absolute text-white text-4xl top-5 left-4 cursor-pointer lg:hidden"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <i className="bi bi-filter-left px-2 bg-gray-900 rounded-md"></i>
+      </span>
+      <div
+        className={`sidebar fixed top-0 bottom-0 lg:left-0 p-2 w-full sm:w-[300px] overflow-y-auto text-center bg-gray-900 ${
+          isOpen ? "" : "hidden"
+        }`}
+      >
+        <div className="text-gray-100 text-xl">
+          <div className="p-2.5 mt-1 flex items-center justify-between">
+            <div className="flex items-center">
+              <i className="bi bi-app-indicator px-2 py-1 rounded-md bg-blue-600"></i>
+              <h1 className="font-bold text-gray-200 text-[15px] ml-3">
+                BookMarkify
+              </h1>
+            </div>
+            <i
+              className="bi bi-x cursor-pointer sm:hidden"
+              onClick={() => setIsOpen(!isOpen)}
+            ></i>
+          </div>
+          <div className="my-2 bg-gray-600 h-[1px]"></div>
+        </div>
+        <div className="p-2.5 flex items-center rounded-md px-4 duration-300 cursor-pointer bg-gray-700 text-white">
+          <i className="bi bi-search text-sm"></i>
+          <input
+            type="text"
+            placeholder="Search"
+            className="text-[15px] ml-4 w-full bg-transparent focus:outline-none"
+          />
+        </div>
+        <div className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
+          <i className="bi bi-house-door-fill"></i>
+          <span className="text-[15px] ml-4 text-gray-200 font-bold">Home</span>
+        </div>
+        <div className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
+          <i className="bi bi-bookmark-fill"></i>
+          <span className="text-[15px] ml-4 text-gray-200 font-bold">
+            Bookmark
+          </span>
+        </div>
+        <div className="my-4 bg-gray-600 h-[1px]"></div>
+        <div className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
+          <i className="bi bi-box-arrow-in-right"></i>
+          <span className="text-[15px] ml-4 text-gray-200 font-bold">
+            Logout
+          </span>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SideBar;
