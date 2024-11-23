9f5a5607cbaede037a54814f04038be109f533b9
Sero <kmcaltiz@gmail.com>
2024-11-23 17:39:54 +0800

    create dashboardcontent component

---
+++ src/components/DashboardContent.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DashboardContent = () => {
+  return (
+    <div className="py-4 px-4 md:pl-[312px] mt-12 md:mt-0">
+      <h1 className="text-2xl">Dashboard Content</h1>
+    </div>
+  );
+};
+
+export default DashboardContent;
