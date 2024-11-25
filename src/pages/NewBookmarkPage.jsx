a0031e5bfdb3eb516424a5b4e572e6d578a3e855
Sero <kmcaltiz@gmail.com>
2024-11-25 11:19:38 +0800

    create page for adding bookmark

--- src/pages/NewBookmarkPage.jsx
+++ src/pages/NewBookmarkPage.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import NewBookmarkForm from "../components/NewBookmarkForm";
 import SideBar from "../components/SideBar";
 
 const NewBookmarkPage = () => {
   return (
     <section>
       <SideBar />
+      <div className="py-4 px-4 md:pl-[312px] mt-14 md:mt-0">
+        <NewBookmarkForm />
+      </div>
     </section>
   );
 };
