c154c8f3a8e0074a6ccf994fc9b3e0e53e8bb568
Sero <kmcaltiz@gmail.com>
2024-11-26 16:34:03 +0800

    empty userbookmark

--- src/context/UserBookmarkContext.jsx
+++ src/context/UserBookmarkContext.jsx
@@ -3,35 +3,7 @@ import { createContext, useState } from "react";
 export const UserBookmarkContext = createContext();
 
 const UserBookmarkProvider = ({ children }) => {
-  const [userBookmarks, setUserBookmarks] = useState([
-    {
-      id: 1,
-      userId: 1,
-      title: "React Documentation",
-      url: "https://reactjs.org/",
-      tags: ["react", "frontend", "documentation"],
-      createdAt: new Date(),
-      isPublic: true,
-    },
-    {
-      id: 2,
-      userId: 1,
-      title: "MDN Web Docs",
-      url: "https://developer.mozilla.org/",
-      tags: ["web-development", "reference", "frontend"],
-      createdAt: new Date(),
-      isPublic: false,
-    },
-    {
-      id: 3,
-      userId: 1,
-      title: "Github",
-      url: "https://developer.mozilla.org/",
-      tags: ["web-development", "reference", "version-control-ystem"],
-      createdAt: new Date(),
-      isPublic: false,
-    },
-  ]);
+  const [userBookmarks, setUserBookmarks] = useState([]);
   const [filteredBookmarks, setFilteredBookmarks] = useState(userBookmarks);
 
   return (
