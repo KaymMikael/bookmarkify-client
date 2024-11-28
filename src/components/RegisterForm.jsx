60a7dc8f284df20e27bedf780ea901712fedebde
Sero <kmcaltiz@gmail.com>
2024-11-28 18:00:46 +0800

    user can now register and add their data to database

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,7 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "./Button";
+import axiosHelper from "../axios/axiosHelper";
 
 const RegisterForm = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const [hasError, setHasError] = useState(false);
+
+  //Function to hide message and reset error
+  const hideMessage = () => {
+    setTimeout(() => {
+      setMessage("");
+      setHasError(false);
+    }, 1300);
+  };
+
+  const handleButtonClick = async () => {
+    //reset message and error
+    hideMessage();
+    if (password !== confirmPassword) {
+      setMessage("Password doesn't match");
+      setHasError(true);
+      hideMessage();
+      return;
+    }
+
+    try {
+      const result = await axiosHelper.post("/auth/register", {
+        name,
+        email,
+        password,
+      });
+      setMessage(result.data.message);
+      console.log(result.data.userId);
+      //Reset inputs
+      setName("");
+      setEmail("");
+      setPassword("");
+      setConfirmPassword("");
+    } catch (e) {
+      setHasError(true);
+      //check any error from the api
+      if (e.response && e.response.data.error) {
+        setMessage(e.response.data.error);
+      }
+      console.log(e.message);
+      hideMessage();
+    }
+  };
+
   return (
     <form onSubmit={(e) => e.preventDefault()} className="space-y-5">
       <div>
@@ -10,6 +60,10 @@ const RegisterForm = () => {
           type="text"
           required
           className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-primary shadow-sm rounded-lg"
+          minLength={4}
+          maxLength={50}
+          value={name}
+          onChange={(e) => setName(e.target.value)}
         />
       </div>
       <div>
@@ -18,6 +72,8 @@ const RegisterForm = () => {
           type="email"
           required
           className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-primary shadow-sm rounded-lg"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
       </div>
       <div>
@@ -26,6 +82,9 @@ const RegisterForm = () => {
           type="password"
           required
           className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-primary shadow-sm rounded-lg"
+          min={8}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
       </div>
       <div>
@@ -34,9 +93,17 @@ const RegisterForm = () => {
           type="password"
           required
           className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-primary shadow-sm rounded-lg"
+          min={8}
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
         />
       </div>
-      <Button text={"Create account"} onClick={() => console.log("clicked")} />
+      {message && (
+        <p className={hasError ? "text-red-500" : "text-green-500"}>
+          {message}
+        </p>
+      )}
+      <Button text={"Create account"} onClick={handleButtonClick} />
     </form>
   );
 };
