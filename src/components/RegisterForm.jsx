e8f5d9c5e13101c381bad6df35c71f544ae840ec
Sero <kmcaltiz@gmail.com>
2024-11-28 18:13:00 +0800

    change debounce import

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { debounce } from "lodash";
+import debounce from "lodash.debounce";
 import Button from "./Button";
 import axiosHelper from "../axios/axiosHelper";
 
