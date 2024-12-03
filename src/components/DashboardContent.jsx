721f05e12dd7a5aed5a55255df716bd4e7337f6e
Sero <kmcaltiz@gmail.com>
2024-12-03 09:48:22 +0800

    use usefetch

--- src/components/DashboardContent.jsx
+++ src/components/DashboardContent.jsx
@@ -1,30 +1,20 @@
-import React, { useContext, useEffect, useState, useCallback } from "react";
 import DashboardHead from "./DashboardHead";
 import DashboardBody from "./DashboardBody";
-import axiosHelper from "../axios/axiosHelper";
 import { useAuth } from "../hooks/useAuth";
 import { UserBookmarkContext } from "../context/UserBookmarkContext";
+import useFetch from "../hooks/useFetch";
+import { useContext, useEffect } from "react";
 
 const DashboardContent = () => {
   const { setUserBookmarks } = useContext(UserBookmarkContext);
-  const [loading, setLoading] = useState(true);
-
   const { user } = useAuth();
-
-  const fetchUserBookmark = useCallback(async () => {
-    try {
-      const result = await axiosHelper.get(`/bookmark/${user.user_id}`);
-      setUserBookmarks(result.data);
-    } catch (e) {
-      console.error(`Error fetching data: ${e}`);
-    } finally {
-      setLoading(false);
-    }
-  }, [user.user_id, setUserBookmarks]);
+  const { data, loading } = useFetch(`/bookmark/${user.user_id}`);
 
   useEffect(() => {
-    fetchUserBookmark();
-  }, [fetchUserBookmark]);
+    if (data && !loading) {
+      setUserBookmarks(data);
+    }
+  }, [data]);
 
   return (
     <div className="py-4 px-4 md:pl-[312px] mt-14 md:mt-0">
