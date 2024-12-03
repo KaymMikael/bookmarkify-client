a012e428d051b08ccd641d6ce9d8051483579dd5
Sero <kmcaltiz@gmail.com>
2024-12-03 19:25:54 +0800

    add query client on providers

--- src/Provider.jsx
+++ src/Provider.jsx
@@ -1,7 +1,14 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AuthProvider from "./context/AuthContext";
 
+const queryClient = new QueryClient();
+
 const Provider = ({ children }) => {
-  return <AuthProvider>{children}</AuthProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>{children}</AuthProvider>
+    </QueryClientProvider>
+  );
 };
 
 export default Provider;
