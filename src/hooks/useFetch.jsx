f251e5c6ac923d6cbcf9b852fa23036e4ba5d548
Sero <kmcaltiz@gmail.com>
2024-12-03 09:48:15 +0800

    create usefetch hook

---
+++ src/hooks/useFetch.jsx
@@ -0,0 +1,29 @@
+import { useCallback, useEffect, useState } from "react";
+import axiosHelper from "../axios/axiosHelper";
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const fetchData = useCallback(
+    async (url) => {
+      try {
+        const result = await axiosHelper.get(url);
+        setData(result.data);
+      } catch (e) {
+        console.error(`Fetch error:${e}`);
+      } finally {
+        setLoading(false);
+      }
+    },
+    [url]
+  );
+
+  useEffect(() => {
+    fetchData(url);
+  }, [url]);
+
+  return { data, loading };
+};
+
+export default useFetch;
