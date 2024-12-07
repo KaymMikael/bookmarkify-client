5e32aff7bbe978a0e399f5bf8b0ef87fdda9655b
Sero <kmcaltiz@gmail.com>
2024-12-07 09:08:56 +0800

    render public bookmarks

--- src/api/BookMark.jsx
+++ src/api/BookMark.jsx
@@ -12,3 +12,7 @@ export const addBookmark = async (bookmarkData) => {
 export const deleteBookmark = async (bookmarkId) => {
   return axiosHelper.delete(`/bookmarks/${bookmarkId}`);
 };
+
+export const fetchPublicBookmarks = async () => {
+  return (await axiosHelper.get("/bookmarks")).data;
+};
--- src/pages/PublicPage.jsx
+++ src/pages/PublicPage.jsx
@@ -1,11 +1,33 @@
 import React from "react";
 import SideBar from "../components/SideBar";
+import { useAuth } from "../hooks/useAuth";
+import { useQuery } from "@tanstack/react-query";
+import { fetchPublicBookmarks } from "../api/BookMark";
+import BookmarkList from "../components/BookmarkList";
 
 const PublicPage = () => {
+  const { user } = useAuth();
+  const {
+    data: publicBookmarks,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ["publicBookmarks"],
+    queryFn: fetchPublicBookmarks,
+    staleTime: Infinity,
+  });
   return (
     <section>
       <SideBar />
-      <div className="py-4 px-4 md:pl-[312px] mt-14 md:mt-0">Public page</div>
+      <div className="py-4 px-4 md:pl-[312px] mt-14 md:mt-0">
+        <div className="max-w-4xl mx-auto">
+          <BookmarkList
+            bookmarks={publicBookmarks}
+            loading={isLoading}
+            user={user}
+          />
+        </div>
+      </div>
     </section>
   );
 };
