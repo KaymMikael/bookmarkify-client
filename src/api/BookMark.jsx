5acf72eaa258d309fb2ee0ec0ec3e2c98313c203
Sero <kmcaltiz@gmail.com>
2024-12-07 15:35:12 +0800

    update url path

--- src/api/BookMark.jsx
+++ src/api/BookMark.jsx
@@ -1,8 +1,12 @@
 import axiosHelper from "../axios/axiosHelper";
 
 export const fetchUserBookmarkByUserId = async (userId) => {
-  const result = await axiosHelper.get(`/bookmarks/${userId}`);
+  const result = await axiosHelper.get(`/bookmarks/${userId}/user-bookmarks`);
   return result.data;
+};
+
+export const fetchBookmarkByBookmarkId = async (bookmarkId) => {
+  return (await axiosHelper.get(`/bookmarks/${bookmarkId}`)).data;
 };
 
 export const addBookmark = async (bookmarkData) => {
