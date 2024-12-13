b68dc046c58faddbac94b279cbf6e1a1a49fdb55
Sero <kmcaltiz@gmail.com>
2024-12-13 16:28:41 +0800

    remove profile from the sidebar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -86,12 +86,6 @@ const SideBar = () => {
             Public
           </span>
         </Link>
-        <div className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white hover:scale-90">
-          <i className="bi bi-person-fill"></i>
-          <span className="text-[15px] ml-4 text-gray-200 font-bold">
-            Profile
-          </span>
-        </div>
         <div className="my-4 bg-gray-600 h-[1px]"></div>
         <Logout />
       </nav>
